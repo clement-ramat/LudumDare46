6f0c6fa910a4ac10571eb5f8e994c4c88ef11676
LadyCookie <bouzerea@etud.insa-toulouse.fr>
2020-04-18 16:33:18 -0400

    main menu done

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public UnityEvent onGameStart;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            onGameStart?.Invoke();
+        }
+    }
+
+    public void loadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
