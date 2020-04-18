9afa6b36bfe52abd2ec89fbd944969852e3e8739
antoria <antoiria91@gmail.com>
2020-04-18 10:17:26 -0400

    LAURA NOoooOn

---
+++ Assets/Scripts/IceCubeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceCubeController : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    [SerializeField]
+    private float baseSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        _controller.Move(movement * Time.deltaTime * baseSpeed);
+    }
+}
