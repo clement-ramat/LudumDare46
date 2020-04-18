3f9bdc3eeae37f9530fe1f1abdad4ef9fd79aa71
antoria <antoiria91@gmail.com>
2020-04-18 14:49:17 -0400

    Barrel roll + master

--- Assets/Scripts/IceCubeController.cs
+++ Assets/Scripts/IceCubeController.cs
@@ -10,16 +10,28 @@ public class IceCubeController : MonoBehaviour
     [SerializeField]
     private float descentSpeed = 1f;
 
+    private float rot;
+
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        rot = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _rb.AddForce(new Vector3(Input.GetAxis("Horizontal") * horizontalSpeed, 0, descentSpeed));
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        _rb.AddForce(new Vector3(horizontalInput * horizontalSpeed, 0, descentSpeed));
+
+        rot += Time.deltaTime;
+
+        if(horizontalInput < 0) _rb.MoveRotation(Quaternion.AngleAxis(180 * rot, Vector3.forward));
+        else if(horizontalInput > 0) _rb.MoveRotation(Quaternion.AngleAxis(180 * rot * -1, Vector3.forward));
+
+
     }
 
     public float GetCurrentVelocity()
