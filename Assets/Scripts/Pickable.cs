abf97c78a6d03e7a65b0f3e66cc90825966989bc
rotagila <victordelenclos@gmail.com>
2020-04-18 23:32:02 -0400

    healthPickUp

--- Assets/Scripts/Pickable.cs
+++ Assets/Scripts/Pickable.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class Pickable : MonoBehaviour
 {
     [Header("Animation")]
-    public float verticalSpeed;
-    public float verticalOffset;
-    public float angularVelocity;
+    public float verticalSpeed = 0.8f;
+    public float verticalOffset = 0.33f;
+    public float angularVelocity = 50;
+    private float startY;
+
+    private bool goingUp = true;
+
+    private void Start()
+    {
+        startY = transform.position.y;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -17,8 +25,30 @@ public class Pickable : MonoBehaviour
         }
     }
 
-    public void Effect(GameObject player)
+    public virtual void Effect(GameObject player)
     {
         Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0, angularVelocity * Time.deltaTime, 0);
+        float adaptedSpeed = verticalSpeed * Mathf.Abs(Mathf.Cos(((transform.position.y - startY) /  (verticalOffset * 1.1f)) * Mathf.PI/2));
+
+        if((adaptedSpeed) < 0.05f)
+        {
+           adaptedSpeed = 0.05f;
+        }
+
+        transform.position += (goingUp ? 1 : -1) * new Vector3(0, adaptedSpeed * Time.deltaTime, 0);
+
+        if(goingUp && transform.position.y >= startY + verticalOffset)
+        {
+            goingUp = false;
+        }
+        else if (!goingUp && transform.position.y <= startY - verticalOffset)
+        {
+            goingUp = true;
+        }
     }
 }
