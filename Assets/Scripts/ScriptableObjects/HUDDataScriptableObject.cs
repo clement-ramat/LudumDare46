ad6b57aef0f7988b335ff8198e9f87b9e6d55f63
Clément Ramat <clement.ramat@gmail.com>
2020-04-20 16:13:39 -0400

    des trucs

--- Assets/Scripts/ScriptableObjects/HUDDataScriptableObject.cs
+++ Assets/Scripts/ScriptableObjects/HUDDataScriptableObject.cs
@@ -8,8 +8,12 @@ public class HUDDataScriptableObject : ScriptableObject
 
     public float timeSinceStart;
 
-    public Transform spawnTransform;
 
-    public Transform goalTransform;
+    public Vector3 spawnPosition;
+
+    public Vector3 goalPosition;
+
+    public Vector3 iceCubePosition;
+
 
 }
